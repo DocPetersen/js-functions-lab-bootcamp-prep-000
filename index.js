21ce3cef50c5c5eba638482eb8d14b62fba0b1be
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-22 22:58:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
-   console.log(`Happy holidays, ${name}!`)
-   return 
-}
+   console.log(`Happy holidays, ${name}!`);
+   return `Happy holidays, ${name}!`;
 }
